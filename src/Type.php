sebastianbergmann/type

commit 1899972564edef716bffc16a5cb77ba56a476fca

Fix CS/WS issues

src/Type.php
@@ -9,14 +9,18 @@
  */
 namespace SebastianBergmann\Type;
 
+use function get_class;
+use function gettype;
+use function strtolower;
+
 abstract class Type
 {
     public static function fromValue($value, bool $allowsNull): self
     {
-        $typeName = \gettype($value);
+        $typeName = gettype($value);
 
         if ($typeName === 'object') {
-            return new ObjectType(TypeName::fromQualifiedName(\get_class($value)), $allowsNull);
+            return new ObjectType(TypeName::fromQualifiedName(get_class($value)), $allowsNull);
         }
 
         $type = self::fromName($typeName, $allowsNull);
@@ -30,7 +34,7 @@ abstract class Type
 
     public static function fromName(string $typeName, bool $allowsNull): self
     {
-        switch (\strtolower($typeName)) {
+        switch (strtolower($typeName)) {
             case 'callable':
                 return new CallableType($allowsNull);
 
@@ -82,7 +86,7 @@ abstract class Type
         return ': ' . $this->asString();
     }
 
-    abstract public function isAssignable(Type $other): bool;
+    abstract public function isAssignable(self $other): bool;
 
     abstract public function name(): string;
 

src/CallableType.php
@@ -9,6 +9,20 @@
  */
 namespace SebastianBergmann\Type;
 
+use function assert;
+use function class_exists;
+use function count;
+use function explode;
+use function function_exists;
+use function is_array;
+use function is_object;
+use function is_string;
+use function strpos;
+use Closure;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionObject;
+
 final class CallableType extends Type
 {
     /**
@@ -73,7 +87,7 @@ final class CallableType extends Type
 
     private function isClosure(ObjectType $type): bool
     {
-        return !$type->className()->isNamespaced() && $type->className()->simpleName() === \Closure::class;
+        return !$type->className()->isNamespaced() && $type->className()->simpleName() === Closure::class;
     }
 
     /**
@@ -82,12 +96,12 @@ final class CallableType extends Type
     private function hasInvokeMethod(ObjectType $type): bool
     {
         $className = $type->className()->qualifiedName();
-        \assert(\class_exists($className));
+        assert(class_exists($className));
 
         try {
-            $class = new \ReflectionClass($className);
+            $class = new ReflectionClass($className);
             // @codeCoverageIgnoreStart
-        } catch (\ReflectionException $e) {
+        } catch (ReflectionException $e) {
             throw new RuntimeException(
                 $e->getMessage(),
                 (int) $e->getCode(),
@@ -105,63 +119,63 @@ final class CallableType extends Type
 
     private function isFunction(SimpleType $type): bool
     {
-        if (!\is_string($type->value())) {
+        if (!is_string($type->value())) {
             return false;
         }
 
-        return \function_exists($type->value());
+        return function_exists($type->value());
     }
 
     private function isObjectCallback(SimpleType $type): bool
     {
-        if (!\is_array($type->value())) {
+        if (!is_array($type->value())) {
             return false;
         }
 
-        if (\count($type->value()) !== 2) {
+        if (count($type->value()) !== 2) {
             return false;
         }
 
-        if (!\is_object($type->value()[0]) || !\is_string($type->value()[1])) {
+        if (!is_object($type->value()[0]) || !is_string($type->value()[1])) {
             return false;
         }
 
         [$object, $methodName] = $type->value();
 
-        return (new \ReflectionObject($object))->hasMethod($methodName);
+        return (new ReflectionObject($object))->hasMethod($methodName);
     }
 
     private function isClassCallback(SimpleType $type): bool
     {
-        if (!\is_string($type->value()) && !\is_array($type->value())) {
+        if (!is_string($type->value()) && !is_array($type->value())) {
             return false;
         }
 
-        if (\is_string($type->value())) {
-            if (\strpos($type->value(), '::') === false) {
+        if (is_string($type->value())) {
+            if (strpos($type->value(), '::') === false) {
                 return false;
             }
 
-            [$className, $methodName] = \explode('::', $type->value());
+            [$className, $methodName] = explode('::', $type->value());
         }
 
-        if (\is_array($type->value())) {
-            if (\count($type->value()) !== 2) {
+        if (is_array($type->value())) {
+            if (count($type->value()) !== 2) {
                 return false;
             }
 
-            if (!\is_string($type->value()[0]) || !\is_string($type->value()[1])) {
+            if (!is_string($type->value()[0]) || !is_string($type->value()[1])) {
                 return false;
             }
 
             [$className, $methodName] = $type->value();
         }
 
-        \assert(isset($className) && \is_string($className) && \class_exists($className));
-        \assert(isset($methodName) && \is_string($methodName));
+        assert(isset($className) && is_string($className) && class_exists($className));
+        assert(isset($methodName) && is_string($methodName));
 
         try {
-            $class = new \ReflectionClass($className);
+            $class = new ReflectionClass($className);
 
             if ($class->hasMethod($methodName)) {
                 $method = $class->getMethod($methodName);
@@ -169,7 +183,7 @@ final class CallableType extends Type
                 return $method->isPublic() && $method->isStatic();
             }
             // @codeCoverageIgnoreStart
-        } catch (\ReflectionException $e) {
+        } catch (ReflectionException $e) {
             throw new RuntimeException(
                 $e->getMessage(),
                 (int) $e->getCode(),

src/IterableType.php
@@ -9,6 +9,12 @@
  */
 namespace SebastianBergmann\Type;
 
+use function assert;
+use function class_exists;
+use function is_iterable;
+use ReflectionClass;
+use ReflectionException;
+
 final class IterableType extends Type
 {
     /**
@@ -35,17 +41,17 @@ final class IterableType extends Type
         }
 
         if ($other instanceof SimpleType) {
-            return \is_iterable($other->value());
+            return is_iterable($other->value());
         }
 
         if ($other instanceof ObjectType) {
             $className = $other->className()->qualifiedName();
-            \assert(\class_exists($className));
+            assert(class_exists($className));
 
             try {
-                return (new \ReflectionClass($className))->isIterable();
+                return (new ReflectionClass($className))->isIterable();
                 // @codeCoverageIgnoreStart
-            } catch (\ReflectionException $e) {
+            } catch (ReflectionException $e) {
                 throw new RuntimeException(
                     $e->getMessage(),
                     (int) $e->getCode(),

src/ObjectType.php
@@ -9,6 +9,9 @@
  */
 namespace SebastianBergmann\Type;
 
+use function is_subclass_of;
+use function strcasecmp;
+
 final class ObjectType extends Type
 {
     /**
@@ -34,11 +37,11 @@ final class ObjectType extends Type
         }
 
         if ($other instanceof self) {
-            if (0 === \strcasecmp($this->className->qualifiedName(), $other->className->qualifiedName())) {
+            if (0 === strcasecmp($this->className->qualifiedName(), $other->className->qualifiedName())) {
                 return true;
             }
 
-            if (\is_subclass_of($other->className->qualifiedName(), $this->className->qualifiedName(), true)) {
+            if (is_subclass_of($other->className->qualifiedName(), $this->className->qualifiedName(), true)) {
                 return true;
             }
         }

src/ReflectionMapper.php
@@ -9,9 +9,15 @@
  */
 namespace SebastianBergmann\Type;
 
+use function assert;
+use function sprintf;
+use ReflectionMethod;
+use ReflectionNamedType;
+use ReflectionUnionType;
+
 final class ReflectionMapper
 {
-    public function fromMethodReturnType(\ReflectionMethod $method): Type
+    public function fromMethodReturnType(ReflectionMethod $method): Type
     {
         if (!$method->hasReturnType()) {
             return new UnknownType;
@@ -19,9 +25,9 @@ final class ReflectionMapper
 
         $returnType = $method->getReturnType();
 
-        \assert($returnType instanceof \ReflectionNamedType || $returnType instanceof \ReflectionUnionType);
+        assert($returnType instanceof ReflectionNamedType || $returnType instanceof ReflectionUnionType);
 
-        if ($returnType instanceof \ReflectionNamedType) {
+        if ($returnType instanceof ReflectionNamedType) {
             if ($returnType->getName() === 'self') {
                 return ObjectType::fromName(
                     $method->getDeclaringClass()->getName(),
@@ -35,7 +41,7 @@ final class ReflectionMapper
                 // @codeCoverageIgnoreStart
                 if ($parentClass === false) {
                     throw new RuntimeException(
-                        \sprintf(
+                        sprintf(
                             '%s::%s() has a "parent" return type declaration but %s does not have a parent class',
                             $method->getDeclaringClass()->getName(),
                             $method->getName(),

src/SimpleType.php
@@ -9,6 +9,8 @@
  */
 namespace SebastianBergmann\Type;
 
+use function strtolower;
+
 final class SimpleType extends Type
 {
     /**
@@ -63,7 +65,7 @@ final class SimpleType extends Type
 
     private function normalize(string $name): string
     {
-        $name = \strtolower($name);
+        $name = strtolower($name);
 
         switch ($name) {
             case 'boolean':

src/TypeName.php
@@ -9,6 +9,12 @@
  */
 namespace SebastianBergmann\Type;
 
+use function array_pop;
+use function explode;
+use function implode;
+use function substr;
+use ReflectionClass;
+
 final class TypeName
 {
     /**
@@ -24,18 +30,18 @@ final class TypeName
     public static function fromQualifiedName(string $fullClassName): self
     {
         if ($fullClassName[0] === '\\') {
-            $fullClassName = \substr($fullClassName, 1);
+            $fullClassName = substr($fullClassName, 1);
         }
 
-        $classNameParts = \explode('\\', $fullClassName);
+        $classNameParts = explode('\\', $fullClassName);
 
-        $simpleName    = \array_pop($classNameParts);
-        $namespaceName = \implode('\\', $classNameParts);
+        $simpleName    = array_pop($classNameParts);
+        $namespaceName = implode('\\', $classNameParts);
 
         return new self($namespaceName, $simpleName);
     }
 
-    public static function fromReflection(\ReflectionClass $type): self
+    public static function fromReflection(ReflectionClass $type): self
     {
         return new self(
             $type->getNamespaceName(),

src/UnionType.php
@@ -9,6 +9,10 @@
  */
 namespace SebastianBergmann\Type;
 
+use function count;
+use function implode;
+use function sort;
+
 final class UnionType extends Type
 {
     /**
@@ -61,9 +65,9 @@ final class UnionType extends Type
             $types[] = $type->name();
         }
 
-        \sort($types);
+        sort($types);
 
-        return \implode('|', $types);
+        return implode('|', $types);
     }
 
     public function allowsNull(): bool
@@ -82,7 +86,7 @@ final class UnionType extends Type
      */
     private function ensureMinimumOfTwoTypes(Type ...$types): void
     {
-        if (\count($types) < 2) {
+        if (count($types) < 2) {
             throw new RuntimeException(
                 'A union type must be composed of at least two types'
             );

commit 687001f9b1284062126f8da88ee8b1d87899b449

Fix BC break introduced in 1899972564edef716bffc16a5cb77ba56a476fca

.php_cs.dist
@@ -187,7 +187,7 @@ return PhpCsFixer\Config::create()
         'protected_to_private' => true,
         'return_assignment' => true,
         'return_type_declaration' => ['space_before' => 'none'],
-        'self_accessor' => true,
+        'self_accessor' => false,
         'semicolon_after_instruction' => true,
         'set_type_to_cast' => true,
         'short_scalar_cast' => true,

src/Type.php
@@ -86,7 +86,7 @@ abstract class Type
         return ': ' . $this->asString();
     }
 
-    abstract public function isAssignable(self $other): bool;
+    abstract public function isAssignable(Type $other): bool;
 
     abstract public function name(): string;
 