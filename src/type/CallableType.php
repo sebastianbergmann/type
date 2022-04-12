07e5c4d0b850fcc4399cb074a5e34c26fb981da7
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-04-12 11:46:25 +0200

    Simplify

--- src/type/CallableType.php
+++ src/type/CallableType.php
@@ -20,7 +20,6 @@ use function is_string;
 use function str_contains;
 use Closure;
 use ReflectionClass;
-use ReflectionException;
 use ReflectionObject;
 
 final class CallableType extends Type
@@ -89,7 +88,7 @@ final class CallableType extends Type
 
     private function isClosure(ObjectType $type): bool
     {
-        return !$type->className()->isNamespaced() && $type->className()->simpleName() === Closure::class;
+        return $type->className()->qualifiedName() === Closure::class;
     }
 
     /**
@@ -98,25 +97,10 @@ final class CallableType extends Type
     private function hasInvokeMethod(ObjectType $type): bool
     {
         $className = $type->className()->qualifiedName();
+
         assert(class_exists($className));
 
-        try {
-            $class = new ReflectionClass($className);
-            // @codeCoverageIgnoreStart
-        } catch (ReflectionException $e) {
-            throw new RuntimeException(
-                $e->getMessage(),
-                (int) $e->getCode(),
-                $e
-            );
-            // @codeCoverageIgnoreEnd
-        }
-
-        if ($class->hasMethod('__invoke')) {
-            return true;
-        }
-
-        return false;
+        return (new ReflectionClass($className))->hasMethod('__invoke');
     }
 
     private function isFunction(SimpleType $type): bool
@@ -173,27 +157,21 @@ final class CallableType extends Type
             [$className, $methodName] = $type->value();
         }
 
-        assert(isset($className) && is_string($className) && class_exists($className));
+        assert(isset($className) && is_string($className));
         assert(isset($methodName) && is_string($methodName));
 
-        try {
-            $class = new ReflectionClass($className);
-
-            if ($class->hasMethod($methodName)) {
-                $method = $class->getMethod($methodName);
-
-                return $method->isPublic() && $method->isStatic();
-            }
-            // @codeCoverageIgnoreStart
-        } catch (ReflectionException $e) {
-            throw new RuntimeException(
-                $e->getMessage(),
-                (int) $e->getCode(),
-                $e
-            );
-            // @codeCoverageIgnoreEnd
+        if (!class_exists($className)) {
+            return false;
         }
 
-        return false;
+        $class = new ReflectionClass($className);
+
+        if (!$class->hasMethod($methodName)) {
+            return false;
+        }
+
+        $method = $class->getMethod($methodName);
+
+        return $method->isPublic() && $method->isStatic();
     }
 }
