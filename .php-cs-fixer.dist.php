25cedd43b45073b511ec8c20f73c114e15c88794
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-01-15 14:47:43 +0100

    Update PHP-CS-Fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -88,6 +88,7 @@ $config->setFinder($finder)
         'function_declaration' => true,
         'function_to_constant' => true,
         'function_typehint_space' => true,
+        'get_class_to_class_keyword' => true,
         'global_namespace_import' => [
             'import_classes' => true,
             'import_constants' => true,
@@ -119,7 +120,11 @@ $config->setFinder($finder)
         'multiline_whitespace_before_semicolons' => true,
         'native_constant_invocation' => false,
         'native_function_casing' => false,
-        'native_function_invocation' => false,
+        'native_function_invocation' => [
+            'include' => [
+                '@internal',
+            ],
+        ],
         'native_function_type_declaration_casing' => true,
         'new_with_braces' => [
             'named_class' => false,
@@ -154,14 +159,14 @@ $config->setFinder($finder)
         'no_superfluous_phpdoc_tags' => [
             'allow_mixed' => true,
         ],
-        'no_trailing_comma_in_list_call' => true,
-        'no_trailing_comma_in_singleline_array' => true,
+        'no_trailing_comma_in_singleline' => true,
         'no_trailing_whitespace' => true,
         'no_trailing_whitespace_in_comment' => true,
         'no_trailing_whitespace_in_string' => true,
         'no_unneeded_control_parentheses' => true,
         'no_unneeded_curly_braces' => true,
         'no_unneeded_final_method' => true,
+        'no_unneeded_import_alias' => true,
         'no_unreachable_default_argument_value' => true,
         'no_unset_cast' => true,
         'no_unset_on_property' => true,
